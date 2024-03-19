17c58626c39531b7304df6f8731e031158d974bd
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2024-03-20 03:41:46 +0530

    Created Weather Page

--- weatherappnative/components/SearchBar.js
+++ weatherappnative/components/SearchBar.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, TextInput, StyleSheet, Dimensions } from 'react-native'
 import React from 'react'
 
 export default function SearchBar() {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.searchBar}>
+        
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+})
--- weatherappnative/components/Weather.js
+++ weatherappnative/components/Weather.js
@@ -9,7 +9,8 @@ export default function Weather( {weatherData} ) {
 
     const { weather,
             name,
-            main: { temp, humidity }
+            main: { temp, humidity },
+            wind: { speed }
     } = weatherData;
     const [{ main }] = weather;
 
@@ -27,6 +28,8 @@ export default function Weather( {weatherData} ) {
         return haze;
     }
 
+    let textColor = backgroundImage !== sunny ? 'white' : 'black';
+
   return (
     <View style={styles.container}>
         <ImageBackground
@@ -37,7 +40,21 @@ export default function Weather( {weatherData} ) {
 
             <SearchBar/>
             <View style={{alignItems:'center'}}>
-                <Text>{name}</Text>
+                <Text style={{...styles.headerText, color: textColor, fontWeight: 'bold', fontSize: 46}}>{name}</Text>
+                <Text style={{...styles.headerText, color: textColor, fontWeight: 'bold'}}>{main}</Text>
+                <Text style={{...styles.headerText, color: textColor}}>{temp} °C </Text>
+            </View>
+
+            <View style={styles.extraInfo}>
+            <View style={styles.info}>
+                <Text style={{ fontSize: 22, color: 'white'}}>Humidity</Text>
+                <Text style={{ fontSize: 22, color: 'white'}}>{humidity} %</Text>
+            </View>
+
+            <View style={styles.info}>
+                <Text style={{ fontSize: 22, color: 'white'}}>Wind Speed</Text>
+                <Text style={{ fontSize: 22, color: 'white'}}>{speed} m/s</Text>
+            </View>
             </View>
 
         </ImageBackground>
@@ -57,6 +74,28 @@ const styles = StyleSheet.create({
     backgroundImage: {
         flex: 1,
         width: Dimensions.get('screen').width
+    },
+
+    headerText:{
+        fontSize: 36,
+        marginTop: 10,
+    },
+
+    extraInfo:{
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        padding: 10
+    },
+
+    info:{
+        width: Dimensions.get('screen').width/2.5,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        padding: 10,
+        borderRadius: 15,
+        justifyContent: 'center',
+
     }
+
   });
   
